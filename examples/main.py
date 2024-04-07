19d1b87d3f26a85165d3345d914269cbc4b49bdc
JiangJianYi <jiangjianyi@gnlab.com>
2024-04-07 16:38:20 +0800

    clean code

--- examples/main.py
+++ examples/main.py
@@ -1,3 +1,4 @@
+import torch
 from examples.autogui.generate_data import gen_data
 from examples.autogui.models import ModelLoader
 
@@ -13,10 +14,28 @@ event = {
     }
 """
 
-n_steps = 3600
-n_features = 6
+X = gen_data()
 
-X = gen_data(n_steps)
 if __name__ == "__main__":
+    model = ModelLoader.load("keymouse-lstm")
+
+    # 提供待预测的数据
+    input_data = torch.tensor(X)
+
+    # 将模型设置为评估模式
+    model.eval()
+
+    # 如果模型在GPU上训练，将数据移到GPU上
+    if torch.cuda.is_available():
+        model = model.cuda()
+        input_data = input_data.cuda()
+
+    # 使用no_grad()进行推理，减少内存消耗
+    with torch.no_grad():
+        predictions = model(input_data)
+
+    # 将预测结果转换回CPU（如果在GPU上训练的话）并转换为numpy格式（如果需要的话）
+    predictions = predictions.cpu().numpy()
+
     model = ModelLoader.load("keymouse-lstm")
     print("out", model.predict(X))
