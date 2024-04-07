2122c79e1674433599d36b56244c19c93bdc386e
thebigbrain <379565986@qq.com>
2024-04-07 21:37:15 +0800

    clean code

---
+++ examples/learning/reinforcement/key_mouse_env.py
@@ -0,0 +1,9 @@
+from examples.learning.reinforcement.env import ReinforcementLearningEnv
+
+
+class KeyMouseBehaviourEnv(ReinforcementLearningEnv):
+    def get_state(self):
+        pass
+
+    def reset(self):
+        pass
