fd9a5e9cb7d48538d4a6daf410758df4164ef17d
JiangJianYi <jiangjianyi@gnlab.com>
2024-04-10 15:58:07 +0800

    screenshot

--- examples/autogui/screenshot.py
+++ examples/autogui/screenshot.py
@@ -13,10 +13,8 @@ gray_image = np.array(screenshot).astype(np.uint8)
 # 将图像转换为二值图像
 binary_image = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)[1]
 
-# 显示图像
-# cv2.imshow("Image", binary_image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+cv2.imwrite(get_screeshot_path("binary"), binary_image)
 
-cv2.imwrite(get_screeshot_path("grey"), gray_image)
-cv2.imwrite(get_screeshot_path("output"), binary_image)
+cv2.imwrite(
+    get_screeshot_path("output"), binary_image, [cv2.IMWRITE_PNG_COMPRESSION, 100]
+)
