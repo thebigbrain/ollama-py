4deaeb649bb0268b5fdf2abc554e8879f60a3e19
JiangJianYi <jiangjianyi@gnlab.com>
2024-04-12 16:48:36 +0800

    clean code

--- xlab/generative_agent/experience.py
+++ xlab/generative_agent/experience.py
@@ -1,8 +1,9 @@
 from xlab.generative_agent.state import Perception
+from xlab.generative_agent.action import Action
 
 
 class Experience:
-    def __init__(self, perception: Perception, action, reward) -> None:
+    def __init__(self, perception: Perception, action: Action, reward: float) -> None:
         self.perception = perception
         self.action = action
         self.reward = reward
--- xlab/generative_agent/similarity.py
+++ xlab/generative_agent/similarity.py
@@ -1,26 +1,26 @@
 from typing import Any
 import numpy as np
 
-from xlab.generative_agent.state import EnvState
+from xlab.generative_agent.state import EnvState, Perception
 
 
 class Similarity:
-    def __call__(self, perception, current_state: EnvState) -> Any:
+    def __call__(self, perception: Perception, current_state: EnvState) -> Any:
         pass
 
 
 class LinearSimilarity(Similarity):
-    def __call__(self, perception, current_state: EnvState):
+    def __call__(self, perception: Perception, current_state: EnvState):
         return np.linalg.norm(current_state - perception)
 
 
 class CosineSimilarity(Similarity):
-    def __call__(self, perception, current_state: EnvState):
+    def __call__(self, perception: Perception, current_state: EnvState):
         return np.dot(current_state, perception) / (
             np.linalg.norm(current_state) * np.linalg.norm(perception)
         )
 
 
 class JacardSimilarity(Similarity):
-    def __call__(self, perception, current_state: EnvState):
+    def __call__(self, perception: Perception, current_state: EnvState):
         return len(current_state & perception) / len(current_state | perception)
